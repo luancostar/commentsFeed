bc860f7fb0c691dacf056a980c4c3ea32a132ed5
Luan Rodrigues <luancostar@gmail.com>
2024-01-25 14:58:59 -0300

    subindo primeiro commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,63 @@
-import Comments from "./components/Comments"
-import NewCommentForm from "./components/NewCommentForm"
-import useCommentsRoll from "./hooks/useCommentsRoll"
-
+import { useState } from "react"
 
 export default function App () {
-  const {comments, addComment, removeComment} = useCommentsRoll ()
+  const [author, setAuthor] = useState("")
+  const [content, setContent] = useState("")
+  const [comments, setComments] = useState([])
+  const handleSubmit = (ev) => { ev.preventDefault()
 
+  const newComment = {
+    id: Math.floor(Math.random() * 1000000),
+    author: author,
+    content: content,
+    createdAt: new Date()
+  }
+  
+  setComments((state) => [newComment, ...state])
+  setAuthor("")
+  setContent("")
+}
 
   return (
     <div id="app">
-      <h1>Biblioteca de Jogos</h1>
-     <NewCommentForm addGame={addComment} />
-       <div className="games">
-        {comments.length > 0 ? 
-        comments.map((game) => (
-        <Comments
-         key={game.id}
-         title={game.title}
-         cover={game.cover}
-         onRemove={() => removeComment(game.id)}
-         />))
-        : (
-          <h2>Parece que ainda não há jogos adicionados</h2>
+      <h2>Seção de Comentários:</h2>
+
+      <form onSubmit={handleSubmit} >
+
+        <label htmlFor="author">Email</label>
+        <input 
+        type="email"
+         id="author" 
+         value={author}
+         onChange={(ev) => setAuthor(ev.target.value)}
+         required />
+        <br />
+        <label htmlFor="content">Comentário:</label>
+        <textarea
+         id="content" 
+         required 
+         value={content}
+         onChange={(ev) => setContent(ev.target.value)}
+         cols="30"
+          rows="6"></textarea>
+        <br />
+        <button>Enviar Comentário</button>
+      </form>
+      <hr />
+      <section id="comments">
+        {comments.length > 0
+        ? (
+          comments.map((comment) => (
+              <div key={comment.id}>
+              <h3>{comment.author}</h3>
+              <span>Em {comment.createdAt.toLocaleString()}</span>
+              <p>{comment.content}</p>
+             </div>
+          ))
+        ) : (
+          <p>Seja o primeiro a comentar! </p>
         )}
-       </div>
-    </div>
+      </section>
+    </div>  
   )
 }
